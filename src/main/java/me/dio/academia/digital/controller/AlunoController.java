5de2b872acb9af36c96c362650e637543892a31c
Mileto Almeida <almeidamileto@gmail.com>
2024-12-15 13:11:26 -0300

    feat: ajustes gerais no projeto e melhorias nas classes de Aluno
    - Ajuste no arquivo application.yml para corrigir configurações do datasource e JPA;
    - Melhoria no README.md com mais informações e instruções detalhadas;
    - Alterações nas classes de Aluno:
      - Correções de método(s) ou lógica(s);
      - Adição de novas propriedades ou validações;
      - Melhoria na organização do código.
    - Reformatação de arquivos para seguir boas práticas de desenvolvimento.

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -1,9 +1,23 @@
 package me.dio.academia.digital.controller;
 
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.service.impl.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
+
+    @Autowired
+    private AlunoServiceImpl service;
+
+    @GetMapping
+    public List<Aluno> getAll() {
+        return service.getAll();
+    }
 }
